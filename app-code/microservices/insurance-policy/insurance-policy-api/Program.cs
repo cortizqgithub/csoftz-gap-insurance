2680012381554d3bf44167ec55738a8a6543b37a
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-04 22:20:10 -0500

    Adds the following changes:
    - history.md It is a release file to keep history of release timestamps.
    - version.txt It is a file to track versioning.
    - .gitignore file is a git ignore rule list.
    - insurance-policy.sln It is a visual studio solution file.
    - appsettings_Development.json Configuration file for Development.
    - appsettings.json Configuration file for Production.,
    - insurance-policy-api.csproj C# project configuration file.
    - Program.cs Entry point for execution.
    - Startup.cs Program startup configuration,.
    - launchSettings.json holds configuration to launch the application.
    
    All of these files are new.

---
+++ app-code/microservices/insurance-policy/insurance-policy-api/Program.cs
@@ -0,0 +1,43 @@
+﻿/*----------------------------------------------------------------------------*/
+/* Source File:   PROGRAM.CS                                                  */
+/* Description:   Program Startup.                                            */
+/* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
+/* Date:          May.04/2018                                                 */
+/* Last Modified: May.04/2018                                                 */
+/* Version:       1.1                                                         */
+/* Copyright (c), 2018 CSoftZ.                                                */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ May.04/2018 COQ  File created.
+ -----------------------------------------------------------------------------*/
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Insurance.Policy.Api
+{
+    /// <summary>
+    /// Program Startup.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary>
+        /// Builds the web host.
+        /// </summary>
+        /// <returns>The web host.</returns>
+        /// <param name="args">Arguments.</param>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ app-code/microservices/insurance-policy/insurance-policy-api/Startup.cs
@@ -0,0 +1,54 @@
+﻿/*----------------------------------------------------------------------------*/
+/* Source File:   STARTUP.CS                                                  */
+/* Description:   Main entry point to configure ASP.NET Core Web Api app.     */
+/* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
+/* Date:          May.04/2018                                                 */
+/* Last Modified: May.04/2018                                                 */
+/* Version:       1.1                                                         */
+/* Copyright (c), 2018 CSoftZ.                                                */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ May.04/2018 COQ  File created.
+ -----------------------------------------------------------------------------*/
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Insurance.Policy.Api
+{
+    /// <summary>
+    /// Main entry point to configure ASP.NET Core Web Api app.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:User.Info.Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">Configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
