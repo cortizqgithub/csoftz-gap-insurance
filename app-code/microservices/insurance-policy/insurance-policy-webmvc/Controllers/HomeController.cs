4cc1ee4c92992c27b51d2cef69f4e7e4fb83d4f4
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-06 16:03:48 -0500

    Initializes the Web APP MVC site

---
+++ app-code/microservices/insurance-policy/insurance-policy-webmvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿/*----------------------------------------------------------------------------*/
+/* Source File:   HOMECONTROLLER.CS                                           */
+/* Description:   Controller for HOME assets.                                 */
+/* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
+/* Date:          May.06/2018                                                 */
+/* Last Modified: May.06/2018                                                 */
+/* Version:       1.1                                                         */
+/* Copyright (c), 2018 CSoftZ.                                                */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ May.06/2018 COQ  File created.
+ -----------------------------------------------------------------------------*/
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using insurance_policy_webmvc.Models;
+
+namespace insurance_policy_webmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ app-code/microservices/insurance-policy/insurance-policy-webmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   ERRORVIEWMODEL.CS                                           */
+/* Description:   Model error view class.                                     */
+/* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
+/* Date:          May.06/2018                                                 */
+/* Last Modified: May.06/2018                                                 */
+/* Version:       1.1                                                         */
+/* Copyright (c), 2018 CSoftZ.                                                */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ May.06/2018 COQ  File created.
+ -----------------------------------------------------------------------------*/
+namespace insurance_policy_webmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
